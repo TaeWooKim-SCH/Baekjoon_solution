bc509e379610490490ed69cb6e920d5586be7336
TaeWooKim-SCH <79956107+TaeWooKim-SCH@users.noreply.github.com>
2022-09-01 09:43:39 +0900

    Add files via upload

--- 백준/12단계_집합과 맵.py
+++ 백준/12단계_집합과 맵.py
@@ -11,6 +11,27 @@ for i in range(M):
         print(0)
 
 
+# 14425: 문자열 집합
+import sys
+N, M = map(int, sys.stdin.readline().split())
+N_set = set()
+M_list = []
+score = 0
+for i in range(N): 
+    S = sys.stdin.readline()
+    N_set.add(S)
+for i in range(M):
+    M_string = sys.stdin.readline()
+    M_list.append(M_string)
+for i in range(M):
+    if M_list[i] in N_set:
+        score += 1
+print(score)
+
+
+# 1620: 나는야 포켓몬 마스터 이다솜
+
+
 
 
 
