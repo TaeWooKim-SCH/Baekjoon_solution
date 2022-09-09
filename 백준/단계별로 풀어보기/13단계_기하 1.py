9e6db60e1ebdbb5ecc34afa07db7641a422e3761
TaeWooKim-SCH <79956107+TaeWooKim-SCH@users.noreply.github.com>
2022-09-09 17:19:06 +0900

    Add files via upload

--- 백준/단계별로 풀어보기/13단계_기하 1.py
+++ 백준/단계별로 풀어보기/13단계_기하 1.py
@@ -38,6 +38,50 @@ for i in answer:
     print(i)
 
 
+# 2477: 참외밭
+import sys
+K = int(sys.stdin.readline())
+filed_dict = dict()
+vertical_list = [] # 세로 방향 리스트
+width_list = [] # 가로 방향 리스트
+direc_list = [] # 방향, 길이 리스트
+inside = 0 # 빼야할 면적
+
+for i in range(6):
+    direc, length = map(int, sys.stdin.readline().split())
+    direc_list.append([direc, length])
+
+    # 바깥 부분 면적
+    if (direc == 3) or (direc == 4):
+        vertical_list.append(length)
+    elif (direc == 1) or (direc == 2):
+        width_list.append(length)
+
+# 안쪽 부분 면적(2-4, 3-2, 1-3, 4-1)
+for i in range(5):
+    if direc_list[i][0] == 1:
+        if direc_list[i + 1][0] == 3:
+            inside = direc_list[i][1] * direc_list[i + 1][1]
+    elif direc_list[i][0] == 2:
+        if direc_list[i + 1][0] == 4:
+            inside = direc_list[i][1] * direc_list[i + 1][1]
+    elif direc_list[i][0] == 3:
+        if direc_list[i + 1][0] == 2:
+            inside = direc_list[i][1] * direc_list[i + 1][1]
+    elif direc_list[i][0] == 4:
+        if direc_list[i + 1][0] == 1:
+            inside = direc_list[i][1] * direc_list[i + 1][1]
+
+# 시작과 끝이 안쪽 면적일때        
+if inside == 0: 
+    inside = direc_list[0][1] * direc_list[-1][1]
+            
+outside = max(vertical_list) * max(width_list) # 밭의 바깥 부분 면적
+print((outside - inside) * K)
+
+
+
+
 # 3053: 택시 기하학
 R = int(input())
 print(round(3.14159265359 * (R ** 2), 6))
